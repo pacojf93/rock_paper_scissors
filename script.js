c2cc9eb15f193a8db0949bf345e54543dc859cab
Paco <127772244+pacojf93@users.noreply.github.com>
2024-12-27 19:39:13 +0100

    added playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 
 function getComputerChoice() {
 
@@ -12,11 +15,61 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let choice = parseInt(prompt("Introduce a valid choice\n"))
+    let choice = prompt("Introduce a valid choice\n")
+    return choice
+}
 
-    switch(choice) {
-        case 0: return "rock"
-        case 1: return "paper"
-        case 2: return "scissors"
+function playRound(humanChoice, computerChoice) {
+    if( humanChoice == "rock" ) {
+        switch(computerChoice) {
+            case "rock":
+                console.log("draw!");
+                break;
+            case "paper":
+                console.log("lose!");
+                computerScore ++;
+                break;
+            case "scissors":
+                console.log("win!");
+                humanScore ++;
+                break;
+        }
+    }
+
+    if( humanChoice == "paper" ) {
+        switch(computerChoice) {
+            case "paper":
+                console.log("draw!");
+                break;
+            case "scissors":
+                console.log("lose!");
+                computerScore ++;
+                break;
+            case "rock":
+                console.log("win!");
+                humanScore ++;
+                break;
+        }
+    }
+
+    if( humanChoice == "scissors" ) {
+        switch(computerChoice) {
+            case "scissors":
+                console.log("draw!");
+                break;
+            case "rock":
+                console.log("lose!");
+                computerScore ++;
+                break;
+            case "paper":
+                console.log("win!");
+                humanScore ++;
+                break;
+        }
     }
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
