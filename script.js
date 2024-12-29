c71435bb84b0ab6e9538cf6080f66c14d4298bf4
Paco <127772244+pacojf93@users.noreply.github.com>
2024-12-29 22:23:00 +0100

    initial commit. Added a basic ui and basic styling, refactored code

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let humanScore = 0
+let computerScore = 0
+
+const buttonGroup = document.querySelector("#button-group")
+const humanScoreDisplay = document.querySelector("#human-score")
+const computerScoreDisplay = document.querySelector("#computer-score")
 
 function getComputerChoice() {
 
@@ -11,73 +17,62 @@ function getComputerChoice() {
 
 }
 
-function getHumanChoice() {
-    let choice = prompt("Introduce a valid choice\n")
-    return choice
+function playRound(humanChoice, computerChoice) {
+    switch(humanChoice) {
+        case "rock":
+            switch(computerChoice) {
+                case "rock":
+                    console.log("draw!");
+                    break;
+                case "paper":
+                    console.log("lose!");
+                    computerScore ++;
+                    break;
+                case "scissors":
+                    console.log("win!");
+                    humanScore ++;
+                    break;
+            }
+        case "paper":
+            switch(computerChoice) {
+                case "paper":
+                    console.log("draw!");
+                    break;
+                case "scissors":
+                    console.log("lose!");
+                    computerScore ++;
+                    break;
+                case "rock":
+                    console.log("win!");
+                    humanScore ++;
+                    break;
+            }
+        case "scissors":
+            if( humanChoice == "scissors" ) {
+                switch(computerChoice) {
+                    case "scissors":
+                        console.log("draw!");
+                        break;
+                    case "rock":
+                        console.log("lose!");
+                        computerScore ++;
+                        break;
+                    case "paper":
+                        console.log("win!");
+                        humanScore ++;
+                        break;
+                }
+            }
+
+    }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+buttonGroup.addEventListener("click", (event) => {
+    const humanChoice = event.target.id
+    const computerChoice = getComputerChoice()
 
-    function playRound(humanChoice, computerChoice) {
-        if( humanChoice == "rock" ) {
-            switch(computerChoice) {
-                case "rock":
-                    console.log("draw!");
-                    break;
-                case "paper":
-                    console.log("lose!");
-                    computerScore ++;
-                    break;
-                case "scissors":
-                    console.log("win!");
-                    humanScore ++;
-                    break;
-            }
-        }
-    
-        if( humanChoice == "paper" ) {
-            switch(computerChoice) {
-                case "paper":
-                    console.log("draw!");
-                    break;
-                case "scissors":
-                    console.log("lose!");
-                    computerScore ++;
-                    break;
-                case "rock":
-                    console.log("win!");
-                    humanScore ++;
-                    break;
-            }
-        }
-    
-        if( humanChoice == "scissors" ) {
-            switch(computerChoice) {
-                case "scissors":
-                    console.log("draw!");
-                    break;
-                case "rock":
-                    console.log("lose!");
-                    computerScore ++;
-                    break;
-                case "paper":
-                    console.log("win!");
-                    humanScore ++;
-                    break;
-            }
-        }
-    }
+    playRound(humanChoice, computerChoice)
 
-    for(let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        playRound(humanSelection, computerSelection);
-    }
-
-}
-
-playGame();
-
+    humanScoreDisplay.textContent = humanScore
+    computerScoreDisplay.textContent = computerScore
+})
