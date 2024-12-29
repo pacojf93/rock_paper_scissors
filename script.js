3f10640f488487fe7c943ae35c9215667c86ae88
Paco <127772244+pacojf93@users.noreply.github.com>
2024-12-29 23:00:59 +0100

    refactored code, added display elements, modified styles

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 let humanScore = 0
 let computerScore = 0
+let roundResult = ''
+
+let humanChoice = ''
+let computerChoice = ''
 
 const buttonGroup = document.querySelector("#button-group")
 const humanScoreDisplay = document.querySelector("#human-score")
 const computerScoreDisplay = document.querySelector("#computer-score")
+const humanChoiceDisplay = document.querySelector("#human-choice")
+const computerChoiceDisplay = document.querySelector("#computer-choice")
+const roundResultDisplay = document.querySelector("#result")
+
+
+function clearDisplay() {
+    humanScore = 0
+    computerScore = 0
+    humanChoice = ''
+    computerChoice = ''
+    roundResult = ''
+}
+
+function renderDisplay() {
+    humanScoreDisplay.textContent = humanScore
+    computerScoreDisplay.textContent = computerScore
+
+    humanChoiceDisplay.textContent = humanChoice
+    computerChoiceDisplay.textContent = computerChoice
+    roundResultDisplay.textContent = roundResult
+}
 
 function getComputerChoice() {
 
@@ -23,56 +48,78 @@ function playRound(humanChoice, computerChoice) {
             switch(computerChoice) {
                 case "rock":
                     console.log("draw!");
+                    roundResult = "draw!"
                     break;
                 case "paper":
                     console.log("lose!");
                     computerScore ++;
+                    roundResult = "lose!"
                     break;
                 case "scissors":
                     console.log("win!");
                     humanScore ++;
+                    roundResult = "win!"
                     break;
             }
+            break
         case "paper":
             switch(computerChoice) {
                 case "paper":
                     console.log("draw!");
+                    roundResult = "draw!"
                     break;
                 case "scissors":
                     console.log("lose!");
                     computerScore ++;
+                    roundResult = "lose!"
                     break;
                 case "rock":
                     console.log("win!");
                     humanScore ++;
+                    roundResult = "win!"
                     break;
             }
+            break
         case "scissors":
-            if( humanChoice == "scissors" ) {
-                switch(computerChoice) {
-                    case "scissors":
-                        console.log("draw!");
-                        break;
-                    case "rock":
-                        console.log("lose!");
-                        computerScore ++;
-                        break;
-                    case "paper":
-                        console.log("win!");
-                        humanScore ++;
-                        break;
-                }
+            switch(computerChoice) {
+                case "scissors":
+                    console.log("draw!");
+                    roundResult = "draw!"
+                    break;
+                case "rock":
+                    console.log("lose!");
+                    computerScore ++;
+                    roundResult = "lose!"
+                    break;
+                case "paper":
+                    console.log("win!");
+                    humanScore ++;
+                    roundResult = "win!"
+                    break;
             }
+            break 
+    }
+}
 
+function checkWinner() {
+    if(humanScore === 5) {
+        clearDisplay()
+        alert("human wins!")
+    }
+    else if (computerScore === 5) {
+        clearDisplay()
+        alert("computer wins!")
     }
 }
 
 buttonGroup.addEventListener("click", (event) => {
-    const humanChoice = event.target.id
-    const computerChoice = getComputerChoice()
+    humanChoice = event.target.id
+    computerChoice = getComputerChoice()
 
     playRound(humanChoice, computerChoice)
 
-    humanScoreDisplay.textContent = humanScore
-    computerScoreDisplay.textContent = computerScore
+    checkWinner()
+
+    renderDisplay()
+    
 })
